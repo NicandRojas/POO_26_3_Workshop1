a1fa10faa84b7f2cea1bd47064938c3f66afaa28
NicandRojas <blitzkriegvonmanstain@gmail.com>
2026-09-21 15:09:52 -0500

    Ejercicio contarPalabras

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -306,12 +306,18 @@ public class Workshop {
     return cadena.equals(invertida);
     }
 
-    // Método que cuenta el número de palabras en una cadena
+  // Método que cuenta el número de palabras en una cadena
+
     public int contarPalabras(String cadena) {
-        // TODO: Implementar el método para contar el número de palabras en una cadena.
-        // Ejemplo: Si cadena = "Este es un test", el resultado debería ser 4.
+
+    if (cadena == null || cadena.trim().isEmpty()) {
         return 0;
     }
+
+    String[] palabras = cadena.trim().split("\\s+");
+
+    return palabras.length;
+}
 
     // Método que convierte una cadena a mayúsculas
     public String convertirAMayusculas(String cadena) {
