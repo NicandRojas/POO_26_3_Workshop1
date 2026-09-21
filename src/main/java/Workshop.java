f27980d46a3041dc71f297aea8f9d383a1c81506
NicandRojas <blitzkriegvonmanstain@gmail.com>
2026-09-21 14:51:43 -0500

    Ejercicio rotarArreglo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -260,10 +260,22 @@ public class Workshop {
     }
 
     // Método que rota un arreglo n posiciones
+
     public int[] rotarArreglo(int[] arreglo, int posiciones) {
-        // TODO: Implementar el método para rotar un arreglo n posiciones.
-        // Ejemplo: Si arreglo = [1, 2, 3, 4, 5] y posiciones = 2, el resultado debería ser [3, 4, 5, 1, 2].
-        return new int[0];
+
+    int[] resultado = new int[arreglo.length];
+
+    if (arreglo.length == 0) {
+        return resultado;
+    }
+
+    posiciones = posiciones % arreglo.length;
+
+    for (int i = 0; i < arreglo.length; i++) {
+        resultado[i] = arreglo[(i + posiciones) % arreglo.length];
+    }
+
+    return resultado;
     }
 
     // Método que cuenta los caracteres en una cadena
