56a99c8c76971b19d920ae0bdf4e0300cfbb247e
NicandRojas <blitzkriegvonmanstain@gmail.com>
2026-09-21 19:07:56 -0500

    Ejercicio AreaDeUnCirculo

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -444,9 +444,13 @@ public class Workshop {
     }
 }
 
-    public double areaCirculo(double radio) {
-        return 0.0;
-    }
+
+
+   // El método debe retornar el area de un circulo
+
+  public double areaCirculo(double radio) {
+    return Math.PI * radio * radio;
+   }
 
     public String zoodiac(int day, int month) {
         return "";
